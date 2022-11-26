13b764f3d196d1e7557dea135bac4a3da37006fc
rowlandwilliams <rowland.williams.94@gmail.com>
2022-11-26 18:04:32 +0000

    remove pointer events when group isnt selected

--- components/ProjectUiComponents/ClusterAnalysis/GroupSelectors/GroupSelector/GroupSelector.tsx
+++ components/ProjectUiComponents/ClusterAnalysis/GroupSelectors/GroupSelector/GroupSelector.tsx
@@ -1,55 +1,63 @@
-import classNames from 'classnames';
-import { selectAll } from 'd3-selection';
-import { useState } from 'react';
+import classNames from "classnames";
+import { selectAll } from "d3-selection";
+import { useState } from "react";
 
 interface Props {
-    group: string;
-    borderClass: string;
-    textClass: string;
-    bgClass: string;
-    groupIndex: number;
+  group: string;
+  borderClass: string;
+  textClass: string;
+  bgClass: string;
+  groupIndex: number;
 }
 
-export const GroupSelector = ({ group, borderClass, textClass, bgClass, groupIndex }: Props) => {
-    const [isActive, setIsActive] = useState(true);
+export const GroupSelector = ({
+  group,
+  borderClass,
+  textClass,
+  bgClass,
+  groupIndex,
+}: Props) => {
+  const [isActive, setIsActive] = useState(true);
 
-    const handleSelectorClick = () => {
-        const groupClass = '.group-' + groupIndex;
+  const handleSelectorClick = () => {
+    const groupClass = ".group-" + groupIndex;
 
-        setIsActive(!isActive);
-        selectAll(groupClass).attr('opacity', isActive ? 0 : 1);
-    };
+    setIsActive(!isActive);
+    selectAll(groupClass)
+      .attr("opacity", isActive ? 0 : 1)
+      .attr("pointer-events", isActive ? "none" : "auto");
+  };
 
-    return (
-        <div
-            key={group}
-            className={classNames(
-                'flex items-center ml-1 px-2 rounded-2xl cursor-pointer transition-all duration-200',
-                {
-                    [borderClass]: isActive,
-                    'border border-gray-400': !isActive,
-                },
-            )}
-            onClick={() => handleSelectorClick()}
-        >
-            <div
-                className={classNames(
-                    'w-2 h-2 mr-1 rounded-full transition-all duration-200',
-                    {
-                        [bgClass]: isActive,
-                        'bg-gray-400': !isActive,
-                    },
-                    `hover:${bgClass}`,
-                )}
-            ></div>
-            <div
-                className={classNames('transition-all duration-200', {
-                    [textClass]: isActive,
-                    'text-gray-400': !isActive,
-                })}
-            >
-                {group}
-            </div>
-        </div>
-    );
+  return (
+    <div
+      key={group}
+      className={classNames(
+        "flex items-center ml-1 px-2 rounded-2xl cursor-pointer transition-all duration-200",
+        {
+          [borderClass]: isActive,
+          "border border-gray-400": !isActive,
+        }
+      )}
+      onClick={() => handleSelectorClick()}
+    >
+      <div
+        className={classNames(
+          "w-2 h-2 mr-1 rounded-full transition-all duration-200",
+          {
+            [bgClass]: isActive,
+            "bg-gray-400": !isActive,
+          },
+          `hover:${bgClass}`
+        )}
+      ></div>
+      <div
+        className={classNames("transition-all duration-200", {
+          [textClass]: isActive,
+          "text-gray-400": !isActive,
+        })}
+      >
+        {group}
+      </div>
+    </div>
+  );
 };
