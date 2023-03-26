5c5c5476a5f4d8426dcc2fdcdbf6be864360a16b
rowlandwilliams <rowland.williams.94@gmail.com>
2023-03-26 11:34:38 +0100

    add priority flag

--- components/DesignProjectTemplate/DesignProjectTemplate.tsx
+++ components/DesignProjectTemplate/DesignProjectTemplate.tsx
@@ -30,6 +30,7 @@ export const DesignProjectTemplate = ({ designProject }: Props) => {
             height={400}
             alt={name}
             className="rounded-md"
+            priority
           />
         )}
         {projectImages?.map(
