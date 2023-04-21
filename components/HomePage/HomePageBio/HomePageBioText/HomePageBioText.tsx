4b7398d7c32f0e9332ef805e50227ebac5e19475
rowlandwilliams <rowland.williams.94@gmail.com>
2023-04-21 16:22:30 +0100

    add kulea link

--- components/HomePage/HomePageBio/HomePageBioText/HomePageBioText.tsx
+++ components/HomePage/HomePageBio/HomePageBioText/HomePageBioText.tsx
@@ -6,21 +6,23 @@ export const HomePageBioText = () => {
       <h1 className="text-white text-lg font-medium">Hi I&apos;m Rowland.</h1>
       <div className="space-y-6 leading-6">
         <p>
-          I&apos;m a Software Engineer interested in
-          Data Visualisation. I connect data,
-          design and technology to build apps that
-          explore and visualise complex information.
+          I&apos;m a Software Engineer interested in Data Visualisation. I
+          connect data, design and technology to build apps that explore and
+          visualise complex information.
         </p>
         <p>
           Currently, I am Engineering Lead at{" "}
-          <Image
-            src={"/home/kulea.svg"}
-            width={70}
-            height={100}
-            alt="kulea"
-            className="inline ml-2 mr-0.5 mb-0.5"
-          />
-          , a Nairobi-based start-up digitising commodity supply chains in East Africa. I&apos;m also available for freelance work.
+          <a href="https://kulea.ai/" target="_blank" rel="noreferrer">
+            <Image
+              src={"/home/kulea.svg"}
+              width={70}
+              height={100}
+              alt="kulea"
+              className="inline ml-2 mr-0.5 mb-0.5"
+            />
+          </a>
+          , a Nairobi-based start-up digitising commodity supply chains in East
+          Africa. I&apos;m also available for freelance work.
         </p>
       </div>
     </section>
