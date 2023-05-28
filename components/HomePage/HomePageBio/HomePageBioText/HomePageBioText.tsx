7cb06f902640990f1b227294af47ee78f227219d
rowlandwilliams <rowland.williams.94@gmail.com>
2023-05-28 19:34:17 +0100

    update bio

--- components/HomePage/HomePageBio/HomePageBioText/HomePageBioText.tsx
+++ components/HomePage/HomePageBio/HomePageBioText/HomePageBioText.tsx
@@ -21,8 +21,7 @@ export const HomePageBioText = () => {
               className="inline ml-2 mr-0.5 mb-0.5"
             />
           </a>
-          , a Nairobi-based start-up digitising commodity supply chains in East
-          Africa. I&apos;m also available for freelance work.
+          , a Nairobi-based start-up providing data, insights and infrastructure for agricultural trading in Africa. I&apos;m also available for freelance work.
         </p>
       </div>
     </section>
